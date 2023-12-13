df436194979199c345f7a3c5fd77c65e7841e9b3
unknown <anthony_rosati@yahoo.com>
2023-12-12 19:44:37 -0600

    Added code to seeds.sql for testing purposes

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS records_db;
 CREATE DATABASE records_db;
 
 USE records_db;
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (name)
+VALUES (
+    "Test Dept"
+);
+
+INSERT INTO role (title, salary, department_id)
+VALUES (
+    "Intern", 40, 1
+);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES (
+    "John", "Doe", 1
+);
